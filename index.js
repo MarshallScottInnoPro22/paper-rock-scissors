070ba8d6d81f68b309a7055433193ecacb7e5c69
MarshallScottInnoPro22 <marshallscottinnopro22@gmail.com>
2023-11-30 15:35:34 -0500

    Created a for loop to keep track of rounds

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("")
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+game();
