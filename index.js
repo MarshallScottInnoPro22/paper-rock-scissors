50bd4008e9dbd7d51f0492ebd0f9445bd437f2d2
MarshallScottInnoPro22 <marshallscottinnopro22@gmail.com>
2023-12-02 15:11:50 -0500

    Created conditionals to check score

--- index.js
+++ index.js
@@ -33,12 +33,54 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+function getPlayerChoice () {
+    let playerChoice = false;
+    while(playerChoice == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null) {
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            playerChoice = true;
+            return choiceInLower;
+        }
+    }
+}
+
 function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+//Variables must be created to store round wins
+// You must use let here instead of const because the variable needs to change
+      console.log("Welcome!");
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("")
+    //a for loop is created to keep track of the number of rounds. There are 5 rounds so i must be set to less than 5
+        const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("----------");
+        //This places a line between the rounds in the console
+        if (checkWinner(playerSelection, computerSelection) == "Player") {
+            scorePlayer++;
+        }
+        else if (checkWinner(playerSelection, computerSelection) == "Computer"){
+            scoreComputer++;
+        }
+    }
+    console.log("Game Over");
+    //This prints after 5 rounds have been played
+    if(scorePlayer > scoreComputer){
+    //This checks the score between the player and the computer
+        console.log("You Win!")
+    }
+    else if(scorePlayer < scoreComputer){
+        console.log("You Lose!")
+    }
+    else {
+        console.log("TIE!");
     }
 }
 
 game();
+//This must be called for the program to work. It contains all the final functions
